9342c1ac0a45d5f3a999db0091fbff346ec43344
Peter <peter.lakatos@outlook.de>
2023-05-28 19:32:21 +0200

    code formated

--- src/app/modules/contact/component/contact/contact.component.ts
+++ src/app/modules/contact/component/contact/contact.component.ts
@@ -8,10 +8,7 @@ import { environment } from 'environments/environment';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  constructor(private formBuilder: FormBuilder) {
-
-    
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   form = this.formBuilder.group({
     name: [
@@ -26,44 +23,36 @@ export class ContactComponent {
     message: ['', Validators.compose([Validators.required])],
   });
 
-  public mailValide: boolean= false;
-  public mailInvalid: boolean= false;
+  public mailValide: boolean = false;
+  public mailInvalid: boolean = false;
   public sendEMail: string = 'Send E-Mail Status';
   public sendEmailText: string = '';
 
+  async isFormValide() {
+    if (this.form.status === 'VALID') {
+      this.sendEmailText = 'Send E-mail was succesfully !';
+      emailjs.init(environment.apiKey);
 
+      let params = {
+        from_name: this.form.value.name,
+        email_id: this.form.value.email,
+        message: this.form.value.message,
+      };
 
- async isFormValide(){ 
+      try {
+        const mail = await emailjs.send(
+          environment.service,
+          environment.template,
+          params
+        );
 
-
-  
-
-  if(this.form.status === 'VALID'){
-    this.sendEmailText = 'Send E-mail was succesfully !';
-    emailjs.init(environment.apiKey);
-
-    let params = {
-      from_name: this.form.value.name,
-      email_id: this.form.value.email,
-      message: this.form.value.message,
-    };
-
-    try {
-      const mail = await emailjs.send(
-        environment.service,
-        environment.template,
-        params
-      );
-
-      this.form.reset();
-    } catch (err) {
-
-      console.log(err);
+        this.form.reset();
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      this.sendEmailText =
+        'Send E-mail was not successfully, Something went Wrong pleas try it again !';
     }
-  }else {
-    this.sendEmailText = 'Send E-mail was not successfully, Something went Wrong pleas try it again !';
-  }
-        
-
   }
 }
